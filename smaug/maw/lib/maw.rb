809f494201ee23ef92bb71b77e2320b316de8507
mooff <mooff@together.beer>
2021-06-30 09:00:20 +0100

    Update to Maw 1.3.2

--- smaug/maw/lib/maw.rb
+++ smaug/maw/lib/maw.rb
@@ -2,18 +2,21 @@
 # https://github.com/togetherbeer/maw
 #
 # @copyright 2021 mooff <mooff@@together.beer>
-# @version 1.2.0
+# @version 1.3.2
 # @license AGPLv3
 
-$args = $gtk.args
 $outputs = $args.outputs
-$state = $args.state
 
 def Maw!
   extend Maw::Tick
 
   include Maw
   include Maw::Init
+  include Maw::Ergonomic
+  include Maw::Helpers
+end
+
+def ergonomic!
   include Maw::Ergonomic
 end
 
@@ -39,8 +42,31 @@ module Maw
           $state.did_maw_init = true
           init
         end
-        @tick&.call
+        if @time_ticks
+          start = Time.now
+          result = @tick&.call
+
+          @tick_times[(@tick_times_i += 1) % @tick_time_history_count] = Time.now - start
+
+          if @tick_time_log and $args.tick_count % (60*5) == 0
+            total = 0
+            for time in @tick_times
+              total += time
+            end
+            average = total / @tick_times.size
+            log_info "[Maw] Average tick time: #{'%.2f' % (average*1000)} ms"
+          end
+        else
+          @tick&.call
+        end
       end
+    end
+
+    def time_ticks! opts={}
+      @time_ticks = (opts[:enable] != false)
+      @tick_times = []
+      @tick_time_history_count = opts[:history] || 64
+      @tick_time_log = opts[:log] != false
     end
   end
 
@@ -63,8 +89,7 @@ module Maw
 
           eval "
             module ::Maw::Ergonomic
-              private
-              def #{name}
+              private def #{name}
                 $args.#{name}
               end
             end"
@@ -81,22 +106,37 @@ module Maw
       Kernel.tick_count
     end
 
-    def controls
+    def controls &blk
       $default_controls ||= ::Maw::Controls.new
+      $default_controls.instance_eval(&blk) if blk
+      $default_controls
     end
   end
 
   Ergonomics = Ergonomic # to be even more ergonomic
 
-  class Controls
-    @@i = 0
+  module Helpers
+    def background! color=[0,0,0]
+      $outputs.background_color = color
+    end
 
+    def sounds
+      $outputs.sounds
+    end
+  end
+
+  class Controls
+    attr_accessor :name
+    
+    @@i = 0    
     def initialize name="Control Set #{@@i+=1}", &blk
       @name = name
 
       instance_exec(&blk) if blk
       self
     end
+
+    def to_s; "[#{name}]"; end
 
     def is? state, device, key
       if device == :mouse # mouse doesn't support state qualifiers like .key_down, .key_held etc
